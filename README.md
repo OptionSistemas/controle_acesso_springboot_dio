66304ecc95b0312ca9117fdd4cf5e8a9fd6e928b
OptionSistemas <optionsistemas@yahoo.com.br>
2021-08-07 13:25:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Projeto Java
+
+## Construindo um sistema de Controle de Acesso com Spring Boot
+Neste projeto foi construindo uma API Rest para controle de ponto e acesso
+dos usuários de uma empresa. 
+
+- Java
+- Spring Boot
+- Hibernate Envers
+- Lombok
+- Swagger
---
+++ src/main/java/com/dio/javalive/JavaliveApplication.java
@@ -0,0 +1,13 @@
+package com.dio.javalive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaliveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaliveApplication.class, args);
+    }
+
+}
